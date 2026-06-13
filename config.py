982d64e987d76f0e9e3f6aca65cfd940b3570a19
OmegaSage <his.righteousness@hotmail.com>
2026-06-12 23:58:15 -0400

    CH2:L4

---
+++ config.py
@@ -0,0 +1,2 @@
+# how many characters we want to read from get_file_content
+chars = 10000
---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+
+import config
+
+MAX_CHARS = config.chars
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        if valid_dir is False:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_dir):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        with open(target_dir) as f:
+            content = f.read(MAX_CHARS)
+            # After reading the first MAX_CHARS...
+            if f.read(1):
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+            return content
+    except Exception as e:
+        return f"Error: {e}"
